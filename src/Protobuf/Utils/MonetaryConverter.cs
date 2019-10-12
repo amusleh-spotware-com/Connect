ccdf6ba5f96c0c0992a38fe4d242a1c15e3dade5
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2019-10-12 13:54:05 +0300

    MonetaryConverter added

---
+++ src/Protobuf/Utils/MonetaryConverter.cs
@@ -0,0 +1,9 @@
+﻿namespace Connect.Protobuf.Utils
+{
+    public static class MonetaryConverter
+    {
+        public static long FromMonetary(long monetaryValue) => monetaryValue / 100;
+
+        public static long ToMonetary(long value) => value * 100;
+    }
+}
