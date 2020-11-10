eda95062ca3c9e9fa3097a239f1ebcbe04647844
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2020-11-10 15:30:53 +0300

    Fixed an issue in volume converter classes

--- src/Protobuf/Utils/MonetaryConverter.cs
+++ src/Protobuf/Utils/MonetaryConverter.cs
@@ -1,9 +1,11 @@
-﻿namespace Connect.Protobuf.Utils
+﻿using System;
+
+namespace Connect.Protobuf.Utils
 {
     public static class MonetaryConverter
     {
         public static double FromMonetary(long monetaryValue) => monetaryValue / 100.0;
 
-        public static long ToMonetary(long value) => value * 100;
+        public static long ToMonetary(double value) => Convert.ToInt64(value * 100);
     }
 }
--- src/Protobuf/Utils/VolumeConverter.cs
+++ src/Protobuf/Utils/VolumeConverter.cs
@@ -4,18 +4,18 @@
     {
         public static double UnitsMonetaryToLots(long volumeInUnitsMonetary, long lotsInUnitsMonetary)
         {
-            double lotsInUnits = MonetaryConverter.FromMonetary(lotsInUnitsMonetary);
+            var lotsInUnits = MonetaryConverter.FromMonetary(lotsInUnitsMonetary);
 
             return MonetaryConverter.FromMonetary(volumeInUnitsMonetary) / lotsInUnits;
         }
 
         public static long LotsToUnitsMonetary(double volumeInLots, long lotsInUnitsMonetary)
         {
-            double lotsInUnits = MonetaryConverter.FromMonetary(lotsInUnitsMonetary);
+            var lotsInUnits = MonetaryConverter.FromMonetary(lotsInUnitsMonetary);
 
             var volumeInUnits = volumeInLots * lotsInUnits;
 
-            return MonetaryConverter.ToMonetary((long)volumeInUnits);
+            return MonetaryConverter.ToMonetary(volumeInUnits);
         }
     }
 }
